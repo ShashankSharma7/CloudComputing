383cd826cc44a8e0e1d08a2984741d4e237c4654
ShashankSharma7 <45518112+ShashankSharma7@users.noreply.github.com>
2020-11-25 14:39:39 +0000

    Update AnagramTest.java

--- AnagramTest.java
+++ AnagramTest.java
@@ -1,7 +1,6 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
-
+import java.io.*;
 import java.util.*;
+import java.net.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -15,23 +14,13 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Anagram {
 	
-	public static String[] stopWords = { "'tis","'twas","a","able","about","across","after","ain't","all","almost","also","am","among","an","and","any","are","aren't","as","at","be","because",
-	"been","but","by","can","can't","cannot","could","could've","couldn't","dear","did","didn't","do","does","doesn't","don't","either","else","ever","every","for","from","get",
-	"got","had","has","hasn't","have","he","he'd","he'll","he's","her","hers","him","his","how","how'd","how'll","how's","however","i","i'd","i'll","i'm","i've","if","in","into",
-	"is","isn't","it","it's","its","just","least","let","like","likely","may","me","might","might've","mightn't","most","must","must've","mustn't","my","neither","no","nor","not",
-	"of","off","often","on","only","or","other","our","own","rather","said","say","says","shan't","she","she'd","she'll","she's","should","should've","shouldn't","since","so",
-	"some","than","that","that'll","that's","the","their","them","then","there","there's","these","they","they'd","they'll","they're","they've","this","tis","to","too","twas","us",
-	"wants","was","wasn't","we","we'd","we'll","we're","were","weren't","what","what'd","what's","when","when","when'd","when'll","when's","where","where'd","where'll","where's",
-	"which","while","who","who'd","who'll","who's","whom","why","why'd","why'll","why's","will","with","won't","would","would've","wouldn't","yet","you","you'd","you'll","you're",
-	"you've","your" };
-	
 	public static class AnagramTestMapper extends
 		Mapper<Object, Text, Text, Text> {   //LongWritable rather than Object?
 			public void map(Object key, Text value, Context context)
 					throws IOException, InterruptedException {
 				StringTokenizer itr = new StringTokenizer(value.toString());
 				while (itr.hasMoreTokens()) {
-					String word = itr.nextToken().replaceAll("[^a-zA-Z0-9]", ""); //replace all on special characters
+					String word = itr.nextToken().replaceAll("[^a-zA-Z0-9]", "");
 					char[] arr = word.toCharArray();
 					Arrays.sort(arr);
 					String wordKey = new String(arr);
@@ -51,12 +40,25 @@ public class Anagram {
 				}
 				ArrayList<String> list = new ArrayList<String>(anagram);
 				
-				for(int i = 0; i < stopWords.length; i++) {
-					if(list.contains(stopWords[i])) {
-						list.remove(stopWords[i]);
+				try {
+					URL url = new URL("https://www.textfixer.com/tutorials/common-english-words-with-contractions.txt");
+					
+					Scanner sc = new Scanner(url.openStream());
+					
+					while (sc.hasNextLine()) {
+						String currentLine = sc.nextLine();
+						String [] words = currentLine.split(",");
+						for(String a : words) {
+							if(list.contains(a)) {
+								list.remove(a);
+							}
+						}
 					}
 				}
-				
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+					
 				Collections.sort(list);
 				testword.set(list.toString());
 				
@@ -68,7 +70,7 @@ public class Anagram {
 	
 	public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "anagram"); //word count change 
+        Job job = Job.getInstance(conf, "anagram");
         job.setJarByClass(Anagram.class);
         job.setMapperClass(AnagramTestMapper.class);
         job.setReducerClass(AnagramTestReducer.class);
